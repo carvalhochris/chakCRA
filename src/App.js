150051aa5a0c2e49d14ae501fabf96669c84142b
Christopher <kriss17388+dev@gmail.com>
2023-02-19 16:37:53 +0000

    color mode set up

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import { useColorMode } from "@chakra-ui/react";
 import { SunIcon, MoonIcon } from "@chakra-ui/icons";
 import { Button } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react";
 
 function App() {
   const { colorMode, toggleColorMode } = useColorMode();
-  
+
   return (
     <div className="App">
       <header className="App-header">
@@ -14,16 +15,16 @@ function App() {
         {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
         </Button>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          We make ideas come to life
-        </p>
+        <Text fontSize="2xl">
+          ChakCRA
+        </Text>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn more
+          Fork me on Github
         </a>
       </header>
     </div>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
+import { ColorModeScript } from "@chakra-ui/react";
+import theme from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
